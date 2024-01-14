1578f84d8cea64615d565e2f981ed3195cce894e
Nhat Nguyen <natt.lefevre@gmail.com>
2024-01-14 00:26:19 -0800

    feat: url

--- libs/constants.ts
+++ libs/constants.ts
@@ -1,4 +1,8 @@
 export const isDevelopment = process.env.NODE_ENV === "development";
-export const origin = isDevelopment
-  ? "http://localhost:3000"
-  : "https://study-next-custom-auth.vercel.app";
+
+export const url = {
+  development: "http://localhost:3000",
+  production: "https://study-next-custom-auth.vercel.app",
+};
+
+export const origin = isDevelopment ? url.development : url.production;
